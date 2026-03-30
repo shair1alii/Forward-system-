c6fc6285889863c46dde0188a99287f90b0f5a05
shair1alii <zawarahmed462@gmail.com>
2026-03-30 23:36:26 +0500

    Update forward.py

--- forward.py
+++ forward.py
@@ -3,7 +3,8 @@ from telethon.sessions import StringSession
 
 api_id = 21367965
 api_hash = "198b8590c4c2656e8bc4e2b721e71416"
-session_str = "1BJWap1sBuzwTc3_Y2x5zFDnADXX02ycXfMtkWnu0Du56qy-_-H0sKaJcOc5t8oYAUzXWDWalcSh-UlzkrjkQpc-oPVPP6TfS5zOn_Czdm958wWR5VUEEdTcTbTFeKgKLh-XVImSLAsQnm8TqniQkpAJCU13fC9Z48-AB-9_OodFQBQi5CZyVZPnO8TmllQDbJ1tfFnjX2Lx6asnVtjcqQRPJFxbcsexu4br_Uj-eRl0GIbh_Uj4j5MBPg69bggLMsY3ZlYzf8dLwTTl8OfOiG519VJ1lR33OZZc1LdE_VoVfXlf1T7Nzsu_DHK93Cwc4QyAQsinS9icFbK4wqXoFn5aMX1ZGmUs="
+session_str = "1BJWap1sBuzwTc3_Y2x5zFDnADXX02ycXfMtkWnu0Du56qy-_-H0sKaJcOc5t8oYAUzXWDWalcSh-UlzkrjkQpc-oPVPP6TfS5zOn_Czdm958wWR5VUEEdTcTbTFeKgKLh-XVImSLAsQnm8TqniQkpAJCU13fC9Z48-AB-9_OodFQBQi5CZyVZPnO8TmllQDbJ1tfFnjX2Lx6asnVtjcqQRPJFxbcsexu4br_Uj-eRl0GIbh_Uj4j5MBPg69bggLMsY3ZlYzf8dLwTTl8OfOiG519VJ1lR33OZZc1LdE_VoVfXlf1T7Nzsu_DHK93Cwc4QyAQsinS9icFbK4wqXoFn5aMX1ZGmUs="  # ⚠️ نیا session ڈالنا
+
 source_group = -1002781143657
 target_group = -1003099447280
 
@@ -12,23 +13,14 @@ client = TelegramClient(StringSession(session_str), api_id, api_hash)
 @client.on(events.NewMessage(chats=source_group))
 async def handler(event):
     try:
-        msg = event.message
-
-        # ✅ اگر text ہو
-        if msg.text:
-            await client.send_message(target_group, msg.text)
-
-        # ✅ اگر photo/video/file ہو
-        elif msg.media:
-            await client.send_file(
-                target_group,
-                msg.media,
-                caption=msg.text or ""
-            )
-
+        # ✅ full forward (buttons + OTP + UI same)
+        await client.forward_messages(
+            target_group,
+            event.message
+        )
     except Exception as e:
         print("Error:", e)
 
-print("✅ Userbot is running...")
+print("✅ Userbot running with buttons support...")
 client.start()
 client.run_until_disconnected()
